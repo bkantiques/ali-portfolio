5553c06a215cb396d3822333a6e64bc5575f6bd1
jreed <jreed0719@gmail.com>
2016-03-20 23:52:42 -0400

    Create array of portfolio items, modify structure

--- src/js/app.js
+++ src/js/app.js
@@ -46,35 +46,373 @@ $(document).ready(function() {
 	}
 
 	function PortfolioImage(item) {
-		this = new PortfolioItem(item);
-		this.imgSrc = new ko.observable(item.imgSrc);
+		var image = new PortfolioItem(item);
+		image.imgSrc = new ko.observable(item.imgSrc);
+		return image;
 	}
 
 	function PortfolioVideo(item) {
-		this = new PortfolioItem(item);
-		this.youtubeId = new ko.observable(item.youtubeId);	
+		var video = new PortfolioItem(item);
+		video.youtubeId = new ko.observable(item.youtubeId);	
+		return video;
 	}
 
 	// Data
-	var a = [{
-		title: 'Blah blah *&(&( HIiii',
-		type: 'image',
-		thumbnail: 'https://www.fillmurray.com/200/300',
-		src: 'https://www.fillmurray.com/400/600'
+	var categories = [{
+			title: 'Drawings',
+			thumbnail: 'drawings/scissors.jpg',
+			parentLink: '',
+			path: 'drawings',
+			images: [
+				{
+					title: 'Scale and Flowers'
+				},
+				{
+					title: 'Detail of Scale and Flowers 1'
+				},
+				{
+					title: 'Detail of Scale and Flowers 2'
+				},
+				{
+					title: 'Newspaper'
+				},
+				{
+					title: 'Scissors'
+				},
+				{
+					title: 'Moose'
+				},
+				{
+					title: 'Ostritch'
+				},
+				{
+					title: 'Sever'
+				},
+				{
+					title: 'Sheraton'
+				},
+				{
+					title: 'Widener'
+				},
+				{
+					title: 'Maintenance Vehicle'
+				},
+				{
+					title: 'Emergency Light'
+				},
+				{
+					title: 'Mom Dad'
+				},
+				{
+					title: 'Detail of Mom Dad'
+				}
+			],
+			videos: [],
+			subcategories: []
 		},
 		{
-		title: 'Blah blahi',
-		type: 'video',
-		thumbnail: 'https://www.fillmurray.com/200/300',
-		src: 'mvVFWN4fXDo'
+			title: 'Paintings',
+			thumbnail: 'paintings/kausalya.jpg',
+			parentLink: '',
+			path: 'paintings',
+			images: [
+				{
+					title: 'Kausalya'
+				},
+				{
+					title: 'Figure Painting 1'
+				},
+				{
+					title: 'Figure Painting 2'
+				},
+				{
+					title: 'Portrait'
+				}
+
+			],
+			videos: [],
+			subcategories: []
 		},
 		{
-		title: 'gffgsfd',
-		type: 'video',
-		thumbnail: 'https://www.fillmurray.com/100/400',
-		src: '6-1ezWYhMEY'
+			title: 'Sets',
+			thumbnail: 'sets/references/references-to-salvador-dali-make-me-hot-4.jpg',
+			parentLink: '',
+			path: 'sets',
+			images: [],
+			videos: [],
+			subcategories: [{
+				title: 'References to Salvador Dali Make Me Hot',
+				thumbnail: 'sets/references/references-to-salvador-dali-make-me-hot-1.jpg',
+				parentLink: 'sets',
+				path: 'sets/references',
+				images: [
+					{
+						title: 'References to Salvador Dali Make Me Hot 1'
+					},
+					{
+						title: 'References to Salvador Dali Make Me Hot 2'
+					},
+					{
+						title: 'References to Salvador Dali Make Me Hot 3'
+					},
+					{
+						title: 'References to Salvador Dali Make Me Hot 4'
+					},
+					{
+						title: 'References to Salvador Dali Make Me Hot 5'
+					},
+					{
+						title: 'References to Salvador Dali Make Me Hot 6'
+					},
+					{
+						title: 'References to Salvador Dali Make Me Hot 7'
+					},
+					{
+						title: 'References to Salvador Dali Make Me Hot 8'
+					},
+					{
+						title: 'References to Salvador Dali Make Me Hot 9'
+					},
+					{
+						title: 'References to Salvador Dali Make Me Hot 10'
+					}
+				],
+				videos: [],
+				subcategories: []
+			},
+			{
+				title: 'The Rake\'s Progress',
+				thumbnail: 'sets/rakes-progress/the-rakes-progress-1.jpg',
+				parentLink: 'sets',
+				path: 'sets/rakes-progress',
+				images: [
+					{
+						title: 'The Rake\'s Progress 1'
+					},
+					{
+						title: 'The Rake\'s Progress 2'
+					},
+					{
+						title: 'The Rake\'s Progress 3'
+					},
+					{
+						title: 'The Rake\'s Progress 4'
+					}
+				],
+				videos: [],
+				subcategories: []
+			},
+			{
+				title: 'Exist',
+				thumbnail: 'sets/exist/',
+				parentLink: 'sets',
+				path: 'sets/exist',
+				images: [],
+				videos: [],
+				subcategories: []
+			},
+			{
+				title: 'Unspoken',
+				thumbnail: 'sets/unspoken/unspoken-2.jpg',
+				parentLink: 'sets',
+				path: 'sets/unspoken',
+				images: [
+					{
+						title: 'Unspoken 1'
+					},
+					{
+						title: 'Unspoken 2'
+					}
+				],
+				videos: [],
+				subcategories: []
+			},
+			{
+				title: 'Animation Sets',
+				thumbnail: 'sets/animation-sets/dies-ovi-palace.jpg',
+				parentLink: 'sets',
+				path: 'animation-sets',
+				images: [
+					{
+						title: 'Dies Ovi Palace'
+					}
+				],
+				videos: [],
+				subcategories: []
+			}]
+		},
+		{
+			title: 'Illustration',
+			thumbnail: 'illustration/cards/strawberry-thieves-1.jpg',
+			parentLink: '',
+			path: 'illustration',
+			images: [],
+			videos: [],
+			subcategories: [{
+				title: 'Cards',
+				thumbnail: 'illustration/cards/strawberry-thieves-1.jpg',
+				parentLink: 'illustration',
+				path: 'illustration/cards',
+				images: [],
+				videos: [],
+				subcategories: [
+					{
+						title: 'Strawberry Thieves',
+						thumbnail: 'illustration/cards/strawberry-thieves-1.jpg',
+						parentLink: 'illustration/cards',
+						path: 'illustration/cards/strawberry-thieves',
+						images: [
+							{
+								title: 'Strawberry Thieves 1'
+							},
+							{
+								title: 'Strawberry Thieves 2'
+							},
+							{
+								title: 'Strawberry Thieves 3'
+							},
+							{
+								title: 'Strawberry Thieves 4'
+							},
+							{
+								title: 'Strawberry Thieves 5'
+							},
+							{
+								title: 'Strawberry Thieves 6'
+							}
+						],
+						videos: [],
+						subcategories: []
+					},
+					{
+						title: 'Tierpark',
+						thumbnail: 'illustration/cards/tierpark/cover.jpg',
+						parentLink: 'illustration/cards',
+						path: 'illustration/cards/tierpark',
+						images: [
+							{
+								title: 'Cover'
+							},
+							{
+								title: 'Anteater'
+							},
+							{
+								title: 'Butterfly'
+							},
+							{
+								title: 'Cheetah and Tortoise'
+							},
+							{
+								title: 'Cockatoo'
+							},
+							{
+								title: 'Crocodile'
+							},
+							{
+								title: 'Giraffe'
+							},
+							{
+								title: 'Goat'
+							},
+							{
+								title: 'Gopher'
+							},
+							{
+								title: 'Jaguar'
+							},
+							{
+								title: 'Lobster'
+							},
+							{
+								title: 'Moose'
+							},
+							{
+								title: 'Ostrich'
+							},
+							{
+								title: 'Peacock'
+							},
+							{
+								title: 'Piglet'
+							},
+							{
+								title: 'Polar Bear'
+							},
+							{
+								title: 'Rhinoceros'
+							},
+							{
+								title: 'Shark'
+							},
+							{
+								title: 'Squirrel'
+							},
+							{
+								title: 'Back Cover and Lion'
+							}
+						],
+						videos: [],
+						subcategories: []
+					},
+					{
+						title: 'Birthday',
+						thumbnail: 'illustration/cards/birthday/hannah.jpg'
+						parentLink: 'illustration/cards',
+						path: 'illustration/cards/birthday',
+						images: [
+							{
+								title: 'Alex'
+							},
+							{
+								title: 'Hannah'
+							},
+							{
+								title: 'Ratchberts 1'
+							},
+							{
+								title: 'Ratchberts 2'
+							},
+							{
+								title: 'Shelley'
+							}
+						],
+						videos: [],
+						subcategories: []
+					}
+				]},
+				{
+					title: 'This Harvard Life',
+					thumbnail: 'illustration/this-harvard-life/do-you-believe-in-magic.jpg',
+					parentLink: 'illustration',
+					path: 'illustration/this-harvard-life',
+					images: [
+						{
+							title: 'A Podcast By Any Other Name'
+						},
+						{
+							title: 'Do You Believe in Magic'
+						}
+					],
+					videos: [],
+					subcategories: []
+				}
+			]
+		},
+		{
+			title: 'Miscellaneous',
+			thumbnail: 'miscellaneous/quilt.jpg',
+			parentLink: '',
+			path: '',
+			images: [
+				{
+					title: 'Quilt'
+				}
+			],
+			videos: [],
+			subcategories: []
 		}
-	];
+
+	];	
 
 	function ViewModel(categories) {
 		
@@ -93,7 +431,6 @@ $(document).ready(function() {
 
 		self.clearSelections = function() {
 			self.selectedPortfolioItem(null);
-			self.selectedSubcategory(null);
 			self.selectedCategory(null);
 		};
 
@@ -108,7 +445,7 @@ $(document).ready(function() {
 		self.searchSubcategoriesById = function(category, id) {
 			var categoryFound = false;
 			var categoryId = category.id();
-			var subcategories = category.subcaegories();
+			var subcategories = category.subcategories();
 
 			/*
 			If id matches this category, set as selected category 
@@ -121,7 +458,7 @@ $(document).ready(function() {
 			// Else search subcategories
 			else {
 				for(var i = 0; i < subcategories.length && !categoryFound; i++) {
-					categoryFound = self.searchSubcategoriesById(subcategories[i]);
+					categoryFound = self.searchSubcategoriesById(subcategories[i], id);
 				}
 			}
 
@@ -141,7 +478,7 @@ $(document).ready(function() {
 
 			// Go through base categories searching each branch 
 			for(var i = 0; i < categories.length && !categoryFound; i++) {
-				categoryFound = self.searchSubcategoriesById(self.catetegories[i]);
+				categoryFound = self.searchSubcategoriesById(categories[i], id);
 			}
 
 		};
@@ -210,7 +547,7 @@ $(document).ready(function() {
 			    var itemId = this.params.itemId.toLowerCase()
 
 		    	// If not the current category
-		    	if(!(self.selectedCategory() && self.selectedCategory().id() && self.selectedCategory().id() == categoryId)) {
+		    	if(!(self.selectedCategory() && self.selectedCategory().id() && self.selectedCategory().id() === categoryId)) {
 		    		// Clear selections
 		    		self.clearSelections();
 
@@ -221,7 +558,7 @@ $(document).ready(function() {
 			    		self.setItemInSelectedCategoryById(itemId);
 			    	}
 		    	}
-		    	else if(!(self.selectedItem() && self.selectedItem().id() && self.selectedItem().id() === itemId)) {
+		    	else if(!(self.selectedPortfolioItem() && self.selectedPortfolioItem().id() && self.selectedPortfolioItem().id() === itemId)) {
 		    		// If current category but not current item, clear selected portfolio item
 		    		self.selectedPortfolioItem(null);
 
@@ -229,13 +566,13 @@ $(document).ready(function() {
 		    	}
 
 		    });
-		    
+
 	    }).run();
 
-	    self.initialize(items);
+	    self.initialize(categories);
 
 	}
 
-	ko.applyBindings(new ViewModel(a));
+	ko.applyBindings(new ViewModel(categories));
 
 });
